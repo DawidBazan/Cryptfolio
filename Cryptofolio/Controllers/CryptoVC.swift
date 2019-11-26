c4ab5c05b8e842fef5d04557902ada850bf80811
Dawid <dawidbazan@icloud.com>
2019-11-26 15:58:09 +0000

    top cell with total, add button in header

--- Cryptofolio/Controllers/CryptoVC.swift
+++ Cryptofolio/Controllers/CryptoVC.swift
@@ -11,10 +11,10 @@ import UIKit
 class CryptoVC: UIViewController {
     
     @IBOutlet var tableView: UITableView!
-    @IBOutlet var totalLbl: UILabel!
     
     var viewModel: CryptoViewModel!
     var cryptoInfo: [CryptoInfo] = []
+    var totalValueString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,7 @@ class CryptoVC: UIViewController {
     func setupView() {
         viewModel.updatedCrypto = { [weak self] crypto in
             self?.cryptoInfo = crypto.info
-            self?.totalLbl.text = self?.viewModel.getTotalValue(for: crypto)
+            self?.totalValueString = self?.viewModel.getTotalValue(for: crypto) ?? ""
             self?.tableView.reloadData()
         }
     }
@@ -32,22 +32,21 @@ class CryptoVC: UIViewController {
 
 extension CryptoVC: UITableViewDelegate, UITableViewDataSource {
     
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let frame = tableView.frame
-//        let button = UIButton(frame: CGRectMake(5, 10, 15, 15))  // create button
-//        button.tag = section
-//        // the button is image - set image
-//        button.setImage(UIImage(named: "remove_button"), forState: UIControlState.Normal)  // assumes there is an image named "remove_button"
-//        button.addTarget(self, action: #selector(TestController.remove(_:)), forControlEvents: .TouchUpInside)  // add selector called by clicking on the button
-//
-//        let headerView = UIView(frame: CGRectMake(0, 0, frame.size.width, frame.size.height))  // create custom view
-//        headerView.addSubview(button)   // add the button to the view
-//
-//        return headerView
-//    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func addButtonToHeader(_ header: UITableViewHeaderFooterView) {
+        let headerWidth = header.frame.width
+        let button = UIButton(frame: CGRect(x: headerWidth - 35, y: 5, width: 20, height: 20))
+        let image = UIImage(named: "add")
+        button.setImage(image, for: .normal)
+        header.addSubview(button)
+    }
     
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         if let headerView = view as? UITableViewHeaderFooterView {
+            addButtonToHeader(headerView)
             if #available(iOS 13.0, *) {
                 headerView.contentView.backgroundColor = .systemBackground
                 headerView.backgroundView?.backgroundColor = .systemBackground
@@ -63,17 +62,44 @@ extension CryptoVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "My Coins"
+        switch section {
+        case 1:
+            return "My Coins"
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 1:
+            return 80
+        default:
+            return 150
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cryptoInfo.count
+        switch section {
+        case 1:
+            return cryptoInfo.count
+        default:
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CryptoCell
-        cell.setupCell(with: cryptoInfo[indexPath.row])
-        return cell
+        switch indexPath.section {
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CryptoCell
+            cell.setupCell(with: cryptoInfo[indexPath.row])
+            return cell
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "totalCell") as! TotalValueCell
+            cell.setupCell(with: totalValueString)
+            return cell
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
