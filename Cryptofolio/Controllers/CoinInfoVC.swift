6fbee8c02cf46b2e9fb114ae4e0480124a0ba3f5
Dawid <dawidbazan@icloud.com>
2019-12-22 11:22:12 +0100

    making edit more intuitive

---
+++ Cryptofolio/Controllers/CoinInfoVC.swift
@@ -0,0 +1,28 @@
+//
+//  CoinInfoVC.swift
+//  Cryptofolio
+//
+//  Created by BZN8 on 22/12/2019.
+//  Copyright © 2019 Dawid Bazan. All rights reserved.
+//
+
+import UIKit
+
+class CoinInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension CoinInfoVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
